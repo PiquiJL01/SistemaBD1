ec489abcca3c9078175d3641124271ad9e2fef66
Jose Luis Gil Zamora <joselgz01@gmail.com>
2021-01-05 20:48:07 -0400

    Update
    Eliminacion de Codigo Basura

--- Ucabmart/Ucabmart/Engine/ConexionBD.cs
+++ Ucabmart/Ucabmart/Engine/ConexionBD.cs
@@ -8,24 +8,12 @@ namespace Ucabmart.Engine
 {
     public abstract class ConexionBD<Tipo, Codigo>
     {
-        /**/
         private const string ConnectionString = "Host = labs-dbservices01.ucab.edu.ve; User Id = grupo5bd1; Password = 123456789; Database = grupo5db";
-        private string Comando;
         public NpgsqlCommand Script;
         public NpgsqlDataReader Reader;
         public NpgsqlConnection Conexion = new NpgsqlConnection(ConnectionString);
 
-        /// <summary>
-        /// Prueba de conexion de la base de datos
-        /// </summary>
-        public void Test()
-        {
-
-        }
-
         #region CRUDs
-
-
         /// <summary>
         /// Inserta en la base de datos
         /// </summary>
@@ -34,19 +22,26 @@ namespace Ucabmart.Engine
         {
             try
             {
+                Conexion.Open();
 
-                ConnectOpenConnection();
-
-                Script = ScriptInsertar();
+                ScriptInsertar();
                 
                 Script.Prepare();
 
                 Script.ExecuteNonQuery();
 
-                CloseConnection();
+                Conexion.Close();
             }
             catch (Exception e)
             {
+                try
+                {
+                    Conexion.Close();
+                }
+                catch (Exception f)
+                {
+
+                }
                 throw e;
             }
         }
@@ -67,218 +62,15 @@ namespace Ucabmart.Engine
         public abstract Tipo Leer(Codigo codigo);
 
 
-        /* CRUDs Originales
-        #region Alamcenes
-        
         /// <summary>
-        /// Obtiene una lista con todos los alamcenes
+        /// Actualiza a informacion del <c>Objeto</c> en la BD
         /// </summary>
-        public List<Almacen> ListaAlmacenes()
-        {
-            List<Almacen> lista = new List<Almacen>();
-
-            try
-            {
-                OpenConnection();
-                
-                Command = "SELECT * FROM almacenes";
-                Script = new NpgsqlCommand(Command, Connection);
-                
-                Reader = Script.ExecuteReader();
-                  
-                //Ejemplo de los headers de cada columna de la tabla
-                //Console.WriteLine($"{rdr.GetName(0),-4} {rdr.GetName(1),-10} {rdr.GetName(2),10}");
-                
-                while (Reader.Read())
-                {
-                    // ejemplo para la consola
-                    //Console.WriteLine($"{rdr.GetInt32(0),-4} {rdr.GetString(1),-10} {rdr.GetInt32(2),10}");
-
-                   // Almacen almacen = new Almacen(Reader.GetInt32(0));
-                 //   lista.Add(almacen);
-                }
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-
-            return lista;
-        }
+        public abstract void Actualizar();
 
         /// <summary>
-        /// Busca un almacen en la <c>BD</c>
+        /// Elimina al <c>objeto</c> de la BD
         /// </summary>
-        /// <param name="codigo">Identificador en la <c>BD</c></param>
-        /// <returns>Almacen Buscado</returns>
-        public Almacen BuscarAlmacen(int codigo)
-        {
-            Almacen almacen = null;
-            try
-            {
-                OpenConnection();
-
-                Command = "SELECT * FROM alamacenes WHERE codigo=@valor";
-                Script = new NpgsqlCommand(Command, Connection);
-
-                Script.Parameters.AddWithValue("valor", 1654);
-                NpgsqlDataReader rdr = Script.ExecuteReader();
-
-                //if (rdr.Read())
-                //{
-                //    almacen = new Almacen(rdr.GetInt32(0));
-                //}
-
-                CloseConnection();
-            }
-            catch (Exception e)
-            {
-
-            }
-
-            return almacen;
-        }
-
-        /// <summary>
-        /// Actualizar informacion del almacen en la <c>BD</c>
-        /// </summary>
-        /// <param name="almacen"></param>
-        public void ActualizarAlmacen(Almacen almacen)
-        {
-        }
-        #endregion
-
-        #region Lugar
-        /// <summary>
-        /// Inserta en la <c>BD</c>
-        /// </summary>
-        /// <param name="lugar">Objeto a insertar</param>
-        public void InsertarLugar(Lugar lugar)
-        {
-            try
-            {
-
-                OpenConnection();
-
-                if (lugar.CodigoUbicacion==null)
-                {
-                    Command = "INSERT INTO lugar (lu_codigo, lu_nombre, lu_tipo, lu_descripcion) VALUES (@codigo, @nombre, @tipo, @descripcion)";
-                    Script = new NpgsqlCommand(Command, Connection);
-
-                    Script.Parameters.AddWithValue("codigo", lugar.Codigo);
-                    Script.Parameters.AddWithValue("nombre", lugar.Nombre);
-                    Script.Parameters.AddWithValue("tipo", lugar.Tipo);
-                    Script.Parameters.AddWithValue("descripcion", lugar.Descripcion);
-                }
-                else
-                {
-                    Command = "INSERT INTO lugar (lu_codigo, lu_nombre, lu_tipo, lu_descripcion, lugar_lu_codigo) VALUES (@codigo, @nombre, @tipo, @descripcion, @lugar)";
-                    Script = new NpgsqlCommand(Command, Connection);
-
-                    Script.Parameters.AddWithValue("codigo", lugar.Codigo);
-                    Script.Parameters.AddWithValue("nombre", lugar.Nombre);
-                    Script.Parameters.AddWithValue("tipo", lugar.Tipo);
-                    Script.Parameters.AddWithValue("descripcion", lugar.Descripcion);
-                    Script.Parameters.AddWithValue("lugar", lugar.CodigoUbicacion);
-                }
-                Script.Prepare();
-
-                Script.ExecuteNonQuery();
-
-                CloseConnection();
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-        }
-
-        /// <summary>
-        /// Obtiene una lista con todos los alamcenes
-        /// </summary>
-        public List<Almacen> ListaLugar()
-        {
-            List<Almacen> lista = new List<Almacen>();
-
-            try
-            {
-                OpenConnection();
-
-                Command = "SELECT * FROM almacenes";
-                Script = new NpgsqlCommand(Command, Connection);
-
-                Reader = Script.ExecuteReader();
-
-                //Ejemplo de los headers de cada columna de la tabla
-                //Console.WriteLine($"{rdr.GetName(0),-4} {rdr.GetName(1),-10} {rdr.GetName(2),10}");
-
-                while (Reader.Read())
-                {
-                    // ejemplo para la consola
-                    //Console.WriteLine($"{rdr.GetInt32(0),-4} {rdr.GetString(1),-10} {rdr.GetInt32(2),10}");
-
-                    // Almacen almacen = new Almacen(Reader.GetInt32(0));
-                    //   lista.Add(almacen);
-                }
-            }
-            catch (Exception e)
-            {
-                CloseConnection();
-                throw e;
-            }
-
-            CloseConnection();
-            return lista;
-        }
-
-        /// <summary>
-        /// Busca un almacen en la <c>BD</c>
-        /// </summary>
-        /// <param name="codigo">Identificador en la <c>BD</c></param>
-        /// <returns>Almacen Buscado</returns>
-        public Almacen BuscarLugar(int codigo)
-        {
-            Almacen almacen = null;
-            try
-            {
-                OpenConnection();
-
-                Command = "SELECT * FROM alamacenes WHERE codigo=@valor";
-                Script = new NpgsqlCommand(Command, Connection);
-
-                Script.Parameters.AddWithValue("valor", 1654);
-                NpgsqlDataReader rdr = Script.ExecuteReader();
-
-                //if (rdr.Read())
-                //{
-                //    almacen = new Almacen(rdr.GetInt32(0));
-                //}
-
-                CloseConnection();
-            }
-            catch (Exception e)
-            {
-
-            }
-
-            return almacen;
-        }
-
-        /// <summary>
-        /// Actualizar informacion del almacen en la <c>BD</c>
-        /// </summary>
-        /// <param name="almacen"></param>
-        public void ActualizarLugar(Almacen almacen)
-        {
-            BuscarAlmacen(1233);
-            if (!(BuscarAlmacen(almacen.Codigo) == null))
-            {
-                OpenConnection();
-                // string
-            }
-        }
-        #endregion
-        */
+        public abstract void Eliminar();
         #endregion
     }
 }
--- Ucabmart/Ucabmart/Engine/Lugar.cs
+++ Ucabmart/Ucabmart/Engine/Lugar.cs
@@ -6,13 +6,16 @@ using System.Web;
 
 namespace Ucabmart.Engine
 {
-    public class Lugar : DBConnection<Lugar, int>
+    public class Lugar : ConexionBD<Lugar, int>
     {
         public int Codigo { get; set; }
         public string Nombre { get; set; }
         public string Tipo { get; set; }
         public string Descripcion { get; set; }
         public int CodigoUbicacion { get; set; }
+
+
+        #region Declaraciones
 
         public Lugar(int codigo, string nombre, TipoLugar tipo, string descripcion, Lugar ubicacion = null)
         {
@@ -50,37 +53,200 @@ namespace Ucabmart.Engine
             }
         }
 
-        public override NpgsqlCommand ScriptInsertar()
+        public Lugar(int codigo)
         {
-            NpgsqlCommand Script;
+            Lugar lugar = Leer(codigo);
+            Codigo = lugar.Codigo;
+            Nombre = lugar.Nombre;
+            Tipo = lugar.Tipo;
+            Descripcion = lugar.Descripcion;
+            CodigoUbicacion = lugar.CodigoUbicacion;
+        }
+
+        private Lugar(int codigo, string nombre, string tipo, string descripcion, int ubicacion)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Tipo = tipo;
+            Descripcion = descripcion;
+            CodigoUbicacion = ubicacion;
+        }
+        #endregion
+
+        #region CRUDs
+        public override void ScriptInsertar()
+        {
             if (CodigoUbicacion == 0)
             {
-                string Comando = "INSERT INTO lugar (lu_codigo, lu_nombre, lu_tipo, lu_descripcion) VALUES (@codigo, @nombre, @tipo, @descripcion)";
-                Script = new NpgsqlCommand(Comando, Connection);
+                string Comando = "INSERT INTO lugar (lu_nombre, lu_tipo, lu_descripcion) VALUES (@codigo, @nombre, @tipo, @descripcion)";
+                Script = new NpgsqlCommand(Comando, Conexion);
 
-                Script.Parameters.AddWithValue("codigo", Codigo);
                 Script.Parameters.AddWithValue("nombre", Nombre);
                 Script.Parameters.AddWithValue("tipo", Tipo);
                 Script.Parameters.AddWithValue("descripcion", Descripcion);
             }
             else
             {
-                string Command = "INSERT INTO lugar (lu_codigo, lu_nombre, lu_tipo, lu_descripcion, lugar_lu_codigo) VALUES (@codigo, @nombre, @tipo, @descripcion, @lugar)";
-                Script = new NpgsqlCommand(Command, Connection);
+                string Command = "INSERT INTO lugar (lu_codigo, lu_nombre, lu_tipo, lu_descripcion, lugar_lu_codigo) " +
+                    "VALUES (@codigo, @nombre, @tipo, @descripcion, @lugar)";
+                Script = new NpgsqlCommand(Command, Conexion);
 
-                Script.Parameters.AddWithValue("codigo", Codigo);
                 Script.Parameters.AddWithValue("nombre", Nombre);
                 Script.Parameters.AddWithValue("tipo", Tipo);
                 Script.Parameters.AddWithValue("descripcion", Descripcion);
                 Script.Parameters.AddWithValue("lugar", CodigoUbicacion);
             }
-
-            return Script;
         }
 
         public override Lugar Leer(int codigo)
         {
-            throw new NotImplementedException();
+            Lugar lugar = null;
+
+            try
+            {
+                Conexion.Open();
+
+                string Comando = "SELECT * FROM alamacenes WHERE codigo=@valor";
+                Script = new NpgsqlCommand(Comando, Conexion);
+
+                Script.Parameters.AddWithValue("valor", 1654);
+                NpgsqlDataReader rdr = Script.ExecuteReader();
+
+                if (rdr.Read())
+                {
+                    lugar = new Lugar(Reader.GetInt32(0), Reader.GetString(1), Reader.GetString(2), Reader.GetString(3), Reader.GetInt32(4));
+                }
+
+                Conexion.Close();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    Conexion.Close();
+                }
+                catch (Exception f)
+                {
+
+                }
+                return null;
+            }
+
+            return lugar;
         }
+
+        public override List<Lugar> Todos()
+        {
+            List<Lugar> lista = new List<Lugar>();
+
+            try
+            {
+                Conexion.Open();
+
+                string Command = "SELECT * FROM lugar";
+                NpgsqlCommand Script = new NpgsqlCommand(Command, Conexion);
+
+                Reader = Script.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    Lugar lugar = new Lugar(Reader.GetInt32(0), Reader.GetString(1), Reader.GetString(2), Reader.GetString(3), Reader.GetInt32(4));
+                    lista.Add(lugar);
+                }
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    Conexion.Close();
+                }
+                catch (Exception f)
+                {
+
+                }
+                return null;
+            }
+
+            return lista;
+        }
+
+        public override void Actualizar()
+        {
+            try
+            {
+                Conexion.Open();
+
+                if (CodigoUbicacion == 0)
+                {
+                    string Comando = "UPDATE lugar SET lu_nombre = @nombre, lu_tipo = @tipo, lu_descripcion = @descripcion WHERE lu_codigo = @codigo";
+                    Script = new NpgsqlCommand(Comando, Conexion);
+
+                    Script.Parameters.AddWithValue("codigo", Codigo);
+                    Script.Parameters.AddWithValue("nombre", Nombre);
+                    Script.Parameters.AddWithValue("tipo", Tipo);
+                    Script.Parameters.AddWithValue("descripcion", Descripcion);
+                }
+                else
+                {
+                    string Command = "UPDATE lugar SET lu_nombre = @nombre, lu_tipo = @tipo, lu_descripcion = @descripcion, lugar_lu_codigo = @lugar " +
+                        "WHERE lu_codigo = @ codigo";
+                    Script = new NpgsqlCommand(Command, Conexion);
+
+                    Script.Parameters.AddWithValue("codigo", Codigo);
+                    Script.Parameters.AddWithValue("nombre", Nombre);
+                    Script.Parameters.AddWithValue("tipo", Tipo);
+                    Script.Parameters.AddWithValue("descripcion", Descripcion);
+                    Script.Parameters.AddWithValue("lugar", CodigoUbicacion);
+                }
+
+                Script.Prepare();
+
+                Script.ExecuteNonQuery();
+
+                Conexion.Close();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    Conexion.Close();
+                }
+                catch (Exception f)
+                {
+
+                }
+            }
+        }
+
+        public override void Eliminar()
+        {
+            try
+            {
+                Conexion.Open();
+
+                string Commando = "DELETE FROM lugar WHERE lu_codigo = @codigo";
+                Script = new NpgsqlCommand(Commando, Conexion);
+
+                Script.Parameters.AddWithValue("codigo", Codigo);
+
+                Script.Prepare();
+
+                Script.ExecuteNonQuery();
+
+                Conexion.Close();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    Conexion.Close();
+                }
+                catch (Exception f)
+                {
+
+                }
+            }
+        }
+        #endregion
     }
 }
