ad8b2f1d5095c3a09d5f999a6ff19873d3b77e24
Jose Luis Gil Zamora <joselgz01@gmail.com>
2021-02-05 21:15:24 -0400

    Report Viewer in Designer
    Se pueden vr los reportes en el diseñador

--- Ucabmart/Ucabmart/Views/Reports/WebForm1.aspx.designer.cs
+++ Ucabmart/Ucabmart/Views/Reports/WebForm1.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace Ucabmart.Views.Reports
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// ReportViewer1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Microsoft.Reporting.WebForms.ReportViewer ReportViewer1;
     }
 }
