9108a14c4a3d1f6a2ae4a3b2286d2de112b1c5c7
José Yepes <j.ricardoyepes@gmail.com>
2021-02-11 11:43:50 -0400

    algunos cambios

--- Ucabmart/Ucabmart/Views/Reports/TopClientesMonto.aspx.cs
+++ Ucabmart/Ucabmart/Views/Reports/TopClientesMonto.aspx.cs
@@ -4,13 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Ucabmart.Engine;
 
 namespace Ucabmart.Views.Reports
 {
     public partial class TopClientesMonto : System.Web.UI.Page
     {
+        public string nombreUsuario { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            this.nombreUsuario = Session["NombreLogin"].ToString();
+
+            Productos.Visible = false;
+            Tiendas.Visible = false;
+            Nomina.Visible = false;
+            Proveedores.Visible = false;
+            Clientes.Visible = false;
+            RolesA.Visible = false;
+
+            string rol = Session["Rol"].ToString();
+            int codigoRol = Int32.Parse(rol);
+            Rol nombreRol = new Rol(codigoRol);
+            List<Permiso> listaPermiso = nombreRol.Permisos();
+
+            foreach (Permiso permiso in listaPermiso)
+            {
+                switch (permiso.Codigo)
+                {
+                    case 1:
+                        Productos.Visible = true;
+                        break;
+                    case 2:
+                        Tiendas.Visible = true;
+                        break;
+                    case 3:
+                        Nomina.Visible = true;
+                        break;
+                    case 4:
+                        Proveedores.Visible = true;
+                        break;
+                    case 5:
+                        Clientes.Visible = true;
+                        break;
+                    case 6:
+                        RolesA.Visible = true;
+                        break;
+
+                }
+            }
 
         }
     }
